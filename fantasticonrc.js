9d2975b05fba47e6a258fd45c29fbcf02db35903
Tim G. Thomas <tim@timgthomas.com>
2022-10-24 12:40:57 -0500

    migrate to fantasticon

---
+++ fantasticonrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  codepoints: {
+    'folder': 0xE000,
+    'folder-fill': 0xE001,
+    'folder-solid': 0xE002,
+    'folder-tests': 0xE003,
+    'folder-lib': 0xE004,
+    'folder-public': 0xE005,
+    'folder-secrets': 0xE006,
+    'folder-bin': 0xE007,
+    'folder-config': 0xE008,
+    'folder-nodemodules': 0xE009,
+    'folder-styles': 0xE00A,
+    'folder-components': 0xE00B,
+    'folder-routes': 0xE00C,
+    'folder-scripts': 0xE00D,
+
+    // Doc
+    'doc': 0xE010,
+    'doc-dashed': 0xE011,
+
+    // Doc: Generic code
+    'doc-braces': 0xE012,
+    'doc-angles': 0xE013,
+    'doc-brackets': 0xE014,
+
+    // Doc: Generic
+    'doc-text': 0xE015,
+    'doc-readme': 0xE016,
+    'doc-config': 0xE017,
+    'doc-graphic': 0xE018,
+    'doc-photo': 0xE019,
+    'doc-license': 0xE01A,
+    'doc-secrets': 0xE01B,
+    'doc-package': 0xE01C,
+    'doc-lock': 0xE01D,
+
+    'doc-html': 0xE020,
+    'doc-styles': 0xE021,
+    'doc-tests': 0xE022,
+    'doc-component': 0xE023,
+  }
+}
